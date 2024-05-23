2c2fd29ff69a665cf24879d8962f8ad9a7361b16
alba <albagraells89@gmail.com>
2024-05-23 11:15:28 +0200

    checkpoint

--- sections/footer.php
+++ sections/footer.php
@@ -1,1 +1,29 @@
-<footer>Footer</footer>
+<footer>
+
+
+<div class="container-0">
+    <h1 class="h1 fs-1 text">Conecta con nosotros</h1>
+
+    <div class="grid-container">
+        <div class="grid-item1">Ctra. de Barcelona, 208 bis, 08205, Sabadell</div>
+        <div class="grid-item1">hello@auxili-vital.cat </div>
+        <div class="grid-item1">+123-456-7890 </div>
+        <div class="grid-item1">www.auxili-vital.cat </div>
+       
+    </div>
+    
+    <div class="grid-item-img"><img  class="img" src="" alt="contacto"> Contacto</div>
+
+
+<div class="grid-container-2">
+    <h4>Conecta con nosotros</h4>
+    <div class="grid-item"><img  class="img" src="" alt="Instagram"></div>
+    <div class="grid-item"><img  class="img" src="" alt="Twiter"></div>
+    <div class="grid-item"><img  class="img"  src="" alt="Facebook"></div>
+    <div class="grid-item"><img  class="img" src="" alt="Linkedin"></div>
+</div>
+
+</div>
+
+
+</footer>
