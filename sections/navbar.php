46debc363327a9f2e3fa8c8c97bd1d995763317d
mcat37 <paltimira23@gmail.com>
2024-05-23 13:01:47 +0200

    arreglar mas errores

--- sections/navbar.php
+++ sections/navbar.php
@@ -2,15 +2,15 @@
 <div class="navbar">
     <div class="navbar-left">
         <a href="#"><h1>Auxili Vital</h1></a>
-        <div class="navbar-right" id="navbar">
-            <a href="#responsabilidad"><h2>Responsabilidad Legal</h2></a>
-            <a href="#pautas"><h2>Pautas P.A.S</h2></a>
-            <a href="#protocolos"><h2>Protocolos</h2></a>
-        </div>
-        <a href="#" class="icon" onclick="openMenu()">
-            <i class="fa fa-bars"></i>
-        </a>
     </div>
+    <div class="navbar-right" id="navbar">
+        <a href="#responsabilidad"><h2>Responsabilidad Legal</h2></a>
+        <a href="#pautas"><h2>Pautas P.A.S</h2></a>
+        <a href="#protocolos"><h2>Protocolos</h2></a>
+    </div>
+    <a href="#" class="icon" onclick="openMenu()">
+        <i class="fa fa-bars"></i>
+    </a>
 </div>
 <script>
     navbar = document.getElementById("navbar");
