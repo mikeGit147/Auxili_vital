526f2e5e579fa22378ca0f69b184826e2f0ffa06
mcat37 <paltimira23@gmail.com>
2024-05-23 09:57:15 +0200

    preparo el css para la posicion

--- sections/navbar.php
+++ sections/navbar.php
@@ -1,9 +1,10 @@
-
-<div>
-    <h1>Auxili Vital</h1>
-</div>
-<div>
-    <h2>Responsabilidad Legal</h2>
-    <h2>Pautas P.A.S</h2>
-    <h2>Protocolos</h2>
+<div class="navbar">
+    <div class="navbar-left">
+        <h1>Auxili Vital</h1>
+    </div>
+    <div class="navbar-right">
+        <h2>Responsabilidad Legal</h2>
+        <h2>Pautas P.A.S</h2>
+        <h2>Protocolos</h2>
+    </div>
 </div>
