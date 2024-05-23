20a388969fcb08d8f47df670601f6691dbc780af
TurpialCode <programasfulltech@gmail.com>
2024-05-23 11:33:49 +0200

    checkPoint

--- sections/responsabilidad.php
+++ sections/responsabilidad.php
@@ -1,1 +1,60 @@
-<h1>Responsabiidad</h1>
+<div class="container-fluid p-5 text-center res-bg">
+    <div class="row align-center">
+        <div class="col position-relative">
+            <div class="position-absolute top-50 start-50 translate-middle w-100">
+                <h2 class="fs-1 text-start fw-bold">Responsabilidad Legal</h2>
+                <p class="fs-5 text text-start pe-5">Cuando prestas primeros auxilios, es importante conocer tus responsabilidades legales para asegurarte de que actúas de forma adecuada y legalmente protegida. Aquí tienes una guía sobre las responsabilidades legales más comunes:</p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="accordion accordion-flush gap-3" id="accordionFlushExample">
+                <div class="accordion-item rounded">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed rounded my-3 py-3" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            Obligación de Auxilio
+                        </button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse rounded collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            La ley establece que tenemos la obligación de auxiliar a una persona en peligro inminente, siempre que esto no ponga en riesgo nuestra propia seguridad. No prestar auxilio puede conllevar sanciones legales.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item rounded">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed rounded my-3 py-3" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            Consentimiento
+                        </button>
+                    </h2>
+                    <div id="flush-collapseTwo" class="accordion-collapse rounded collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            Antes de prestar primeros auxilios, es importante obtener el consentimiento de la persona afectada, si está consciente y capaz de darlo. Si está inconsciente, el consentimiento se asume implícitamente. </div>
+                    </div>
+                </div>
+                <div class="accordion-item rounded">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed rounded my-3 py-3" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                            Responsabilidad Civil
+                        </button>
+                    </h2>
+                    <div id="flush-collapseThree" class="accordion-collapse rounded collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            Los primeros auxilios deben prestarse de manera razonable y según nuestros conocimientos. Los errores graves por negligencia pueden derivar en responsabilidades civiles.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item rounded">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed rounded my-3 py-3" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                            Protección Legal
+                        </button>
+                    </h2>
+                    <div id="flush-collapseFour" class="accordion-collapse rounded collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            Muchos países ofrecen protección legal a los socorristas para evitar que sean demandados mientras actúen de buena fe y dentro de los límites de su formación. </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
