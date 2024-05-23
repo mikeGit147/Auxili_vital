5dcadb92ed62e0c900317d90ce4cd650f44545a7
mcat37 <paltimira23@gmail.com>
2024-05-23 10:44:45 +0200

    cambios en css y php

--- sections/navbar.php
+++ sections/navbar.php
@@ -1,10 +1,10 @@
 <div class="navbar">
-    <div class="navbar-left">
+    <div class="navbar-left" id="navbar">
         <h1>Auxili Vital</h1>
     </div>
     <div class="navbar-right">
-        <h2>Responsabilidad Legal</h2>
-        <h2>Pautas P.A.S</h2>
-        <h2>Protocolos</h2>
+        <a href="#responsabilidad"><h2>Responsabilidad Legal</h2></a>
+        <a href="#pautas"><h2>Pautas P.A.S</h2></a>
+        <a href="#protocolos"><h2>Protocolos</h2></a>
     </div>
 </div>
