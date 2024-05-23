3b842ee225f81c252137fd6bd808ffce68dc8340
mcat37 <paltimira23@gmail.com>
2024-05-23 11:38:08 +0200

    añadidos bottones activos, background terminado, posiciones establecidas

--- sections/hero.php
+++ sections/hero.php
@@ -1,13 +1,13 @@
 <?php
 include  __DIR__ . "/navbar.php";
 ?>
-<div class="hero-container">
-    <div class="title">
-        <h1>Aprende Primeros Auxilios</h1>
-    </div>
-    <div class="hero-button">
-        <button class="button-video">
-            <a href="#" target="_blank">Ver Video</a>
-        </button>
+<div class="hero-background">
+    <div class="hero-container">
+        <div class="title">
+            <h1>Aprende<br>Primeros Auxilios</h1>
+        </div>
+        <div class="hero-button">
+            <button class="button-video">Ver Video</button>
+        </div>
     </div>
 </div>
