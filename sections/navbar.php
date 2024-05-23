a2b373efc3c258089ad725fd775314679d321e14
mcat37 <paltimira23@gmail.com>
2024-05-23 13:00:16 +0200

    arreglar errores

--- sections/navbar.php
+++ sections/navbar.php
@@ -1,28 +1,26 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <div class="navbar">
     <div class="navbar-left">
         <a href="#"><h1>Auxili Vital</h1></a>
-    </div>
-    <div class="navbar-right" id="navbar">
-        <a href="#responsabilidad"><h2>Responsabilidad Legal</h2></a>
-        <a href="#pautas"><h2>Pautas P.A.S</h2></a>
-        <a href="#protocolos"><h2>Protocolos</h2></a>
+        <div class="navbar-right" id="navbar">
+            <a href="#responsabilidad"><h2>Responsabilidad Legal</h2></a>
+            <a href="#pautas"><h2>Pautas P.A.S</h2></a>
+            <a href="#protocolos"><h2>Protocolos</h2></a>
+        </div>
         <a href="#" class="icon" onclick="openMenu()">
             <i class="fa fa-bars"></i>
         </a>
     </div>
 </div>
-<?php
-/* function openMenu() {
-    $navbar = document.getElementById("navbar");
-    $icon = document.getElementByClass("icon");
-    icon.style.display= "none";
-    navbar.style.display = "block";
+<script>
+    navbar = document.getElementById("navbar");
+    icon = document.getElementByClass("icon");
+    function openMenu() {
+  if (navbar.className === "navbar") {
+    navbar.className += "responsive";
+  } else {
+    navbar.className = "navbar";
+  }
 }
-function closMenu() {
-    $navbar = document.getElementById("navbar");
-    $icon = document.getElementByClass("icon");
-    icon.style.display= "block";
-    navbar.style.display = "none";
- } */
- ?>
+</script>
+
