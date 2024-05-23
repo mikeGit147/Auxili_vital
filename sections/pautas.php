faf923c9244a7806ce099e2df5d2b567365c9614
Miguel <zcategrales@gmail.com>
2024-05-23 12:59:38 +0200

    minim_deseable

--- sections/pautas.php
+++ sections/pautas.php
@@ -3,14 +3,14 @@
 
     <div class="pas-container">
         <div class="pas-image-container">
-            <img src="2.jpg" alt="Imagen 1">
+            <img src="./assets/img/2.jpg" alt="Imagen 1">
             <div class="pas-caption"><div class="pas-interactive-box">
             <p class="pas-message">Asegurar la zona y a ti mismo.</p>
             <p class="pas-message">Evaluar a los heridos y moverlos a un lugar seguro (si es posible).</p>
         </div></div>
         </div>
         <div class="pas-image-container">
-            <img src="3.jpg" alt="Imagen 2">
+            <img src="./assets/img/3.jpg" alt="Imagen 2">
             <div class="pas-caption"><div class="pas-interactive-box">
                 <p class="pas-message">Llamar al 112.</p>
                 <p class="pas-message">Informar con precisión.</p>
@@ -18,7 +18,7 @@
             </div></div>
         </div>
         <div class="pas-image-container">
-            <img src="4.jpg" alt="Imagen 3">
+            <img src="./assets/img/4.jpg" alt="Imagen 3">
             <div class="pas-caption"><div class="pas-interactive-box">
                 <p class="pas-message">Brindar primeros auxilios básicos (si estás capacitado).</p>
                 <p class="pas-message">Tranquilizar a las víctimas. No mover heridos graves.</p>
@@ -26,11 +26,11 @@
         </div>
     </div>
     <div class="pas-interactive-boxes">
-        <img src="8.jpg" alt="Imagen 3">
+        <img src="./assets/img/8.jpg" alt="Imagen 3">
         <h3 class="pas-h3">PROTEGER</h3>
-        <img src="8.jpg" alt="Imagen 3">
+        <img src="./assets/img/8.jpg" alt="Imagen 3">
         <h3 class="pas-h3">AVISAR</h3>
-        <img src="8.jpg" alt="Imagen 3">
+        <img src="./assets/img/8.jpg" alt="Imagen 3">
         <h3 class="pas-h3">SOCORRER</h3>
     </div>
     <!-- <div class="interactive-boxes">
