3945690d7e7b2defd07428efada94ba2b5200106
Miguel <zcategrales@gmail.com>
2024-05-23 12:45:29 +0200

    checkpoint

--- sections/pautas.php
+++ sections/pautas.php
@@ -1,1 +1,53 @@
-<h1>Pautas</h1>
+<div  class="pas-body">
+    <h1 class="pas-h1">Pauta general d’actuació P.A.S.</h1>
+
+    <div class="pas-container">
+        <div class="pas-image-container">
+            <img src="2.jpg" alt="Imagen 1">
+            <div class="pas-caption"><div class="pas-interactive-box">
+            <p class="pas-message">Asegurar la zona y a ti mismo.</p>
+            <p class="pas-message">Evaluar a los heridos y moverlos a un lugar seguro (si es posible).</p>
+        </div></div>
+        </div>
+        <div class="pas-image-container">
+            <img src="3.jpg" alt="Imagen 2">
+            <div class="pas-caption"><div class="pas-interactive-box">
+                <p class="pas-message">Llamar al 112.</p>
+                <p class="pas-message">Informar con precisión.</p>
+                <p class="pas-message">Seguir instrucciones.</p>
+            </div></div>
+        </div>
+        <div class="pas-image-container">
+            <img src="4.jpg" alt="Imagen 3">
+            <div class="pas-caption"><div class="pas-interactive-box">
+                <p class="pas-message">Brindar primeros auxilios básicos (si estás capacitado).</p>
+                <p class="pas-message">Tranquilizar a las víctimas. No mover heridos graves.</p>
+            </div></div>
+        </div>
+    </div>
+    <div class="pas-interactive-boxes">
+        <img src="8.jpg" alt="Imagen 3">
+        <h3 class="pas-h3">PROTEGER</h3>
+        <img src="8.jpg" alt="Imagen 3">
+        <h3 class="pas-h3">AVISAR</h3>
+        <img src="8.jpg" alt="Imagen 3">
+        <h3 class="pas-h3">SOCORRER</h3>
+    </div>
+    <!-- <div class="interactive-boxes">
+        <div class="interactive-box">
+            <p class="message">Asegurar la zona y a ti mismo.</p>
+            <p class="message">Evaluar a los heridos y moverlos a un lugar seguro (si es posible).</p>
+        </div>
+        <div class="interactive-box">
+            <p class="message">Llamar al 112.</p>
+            <p class="message">Informar con precisión.</p>
+            <p class="message">Seguir instrucciones.</p>
+        </div>
+        <div class="interactive-box">
+            <p class="message">Brindar primeros auxilios básicos (si estás capacitado).</p>
+            <p class="message">Tranquilizar a las víctimas. No mover heridos graves.</p>
+        </div>
+    </div> -->
+
+    <h2 class="pas-h2">PAS: Un protocolo clave para salvar vidas.</h2>
+</div>
