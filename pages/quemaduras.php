81af1e94689a5c579a7d20ac2d2aedfbc59a1b25
TurpialCode <programasfulltech@gmail.com>
2024-05-30 12:18:21 +0200

    feat(proyecto): final

--- pages/quemaduras.php
+++ pages/quemaduras.php
@@ -5,14 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <title>Auxili Vital | Quemaduras</title>
 </head>
 
@@ -37,6 +32,10 @@
                 Refredar la zona afectada amb aigua tèbia durant 10-20 minuts.
                 Cobrir la cremada amb una gasa estèril i no aplicar pomades o o
                 lis</p>
+        </div>
+        <div>
+            <h2>Evitar:</h2>
+            <p>Remover ropa que esté incrustada a la zona quemada.</p>
         </div>
     </div>
 
--- sections/hero.php
+++ sections/hero.php
@@ -2,12 +2,14 @@
 include  __DIR__ . "/navbar.php";
 ?>
 <div class="hero-background">
-    <div class="hero-container"> 
+    <div class="hero-container">
         <div class="title">
-            <h1>Aprende<br>Primeros Auxilios</h1>
+            <h1>Aprende Haciendo<br>Primeros Auxilios</h1>
         </div>
         <div class="hero-button">
-            <button class="button-video">Ver Video</button>
+            <a href="https://www.youtube.com/watch?v=qkpNosF3cMY" target="_blank">
+                <button class="button-video">Ver Video</button>
+            </a>
         </div>
     </div>
 </div>
